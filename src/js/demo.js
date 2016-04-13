e7b5e023e52656e27cb43819cae9c4f67d07c2c5
akshat1 <akshat.sh@gmail.com>
2016-04-12 23:54:49 -0700

    Updated for simia-grid v0.1.0

--- src/js/demo.js
+++ src/js/demo.js
@@ -8,96 +8,94 @@ var SimianGrid = require('simian-grid');
 window.React = React;
 window.ReactDOM = ReactDOM;
 
+var MAX_ROWS = 1000000;
 
-function makeDataModel() {
-  var columnDefinition = [
-    {
-      title: 'Count',
-      className: 'count'
-    }, {
-      title: 'First',
-      className: 'first'
-    }, {
-      title: 'Column Two',
-      className: 'column-two'
-    }, {
-      title: 'Tre',
-      className: 'tre'
-    }, {
-      title: 'Double Two',
-      className: 'double-two'
-    }, {
-      title: 'No Name 001'
-    }, {
-      title: 'No Name 002'
-    }
-  ];
+// used by datagen to produce sample data
+var dataTemplate = [{
+  dataType: 'NUMBER',
+  isCount: true
+}, {
+  dataType: 'STRING',
+  length: 12
+}, {
+  dataType: 'STRING',
+  length: 8
+}, {
+  dataType: 'NUMBER',
+  length: 5
+}, {
+  dataType: 'NUMBER',
+  length: 5
+}, {
+  dataType: 'STRING',
+  length: 8
+}, {
+  dataType: 'NUMBER',
+  length: 8
+}];
 
-  var rows = generateData(window.navigator.userAgent.indexOf('Firefox') === -1 ? 1000000 : 100000, [{
-    dataType : 'NUMBER',
-    isCount  : true
-  }, {
-    dataType : 'STRING',
-    length   : 12
-  }, {
-    dataType : 'STRING',
-    length   : 8
-  }, {
-    dataType : 'NUMBER',
-    length   : 5
-  }, {
-    dataType : 'NUMBER',
-    length   : 5
-  }, {
-    dataType : 'STRING',
-    length   : 8
-  }, {
-    dataType : 'NUMBER',
-    length   : 8
-  }
-  ]);
 
-  return {
-    columnDefinition : columnDefinition,
-    rowHeight        : 39,
-    rows             : rows
-  };
+// Used by simian-grid to render column headers etc.
+var columnDefinition = [{
+  title: 'Count',
+  className: 'count'
+}, {
+  title: 'First',
+  className: 'first'
+}, {
+  title: 'Column Two',
+  className: 'column-two'
+}, {
+  title: 'Tre',
+  className: 'tre'
+}, {
+  title: 'Double Two',
+  className: 'double-two'
+}, {
+  title: 'No Name 001'
+}, {
+  title: 'No Name 002'
+}];
+
+
+function getRows(from, num) {
+  return new Promise(function(resolve, reject) {
+    var rows = generateData(num, dataTemplate);
+    if(from !== 0)
+      rows.forEach(function(row) {
+        row[0] += from; //We already know this is the count; So a little hack instead of changing datagen for now
+      });
+    // Simulate data fetch lag
+    setTimeout(function() {
+      resolve(rows);
+    }, Math.random() * 100);
+  });
 }
 
 
-function insertStyleRules() {
-  var sheetElement = document.createElement('style');
-  sheetElement.appendChild(document.createTextNode(''));
-  document.head.appendChild(sheetElement);
-
-  var sheet = document.styleSheets[0];
-  sheet.insertRule(".even { background: #DDEEFF; }", 0);
-  sheet.insertRule(".odd { background: #FFF; }", 0);
-  sheet.insertRule(".header { background: #1ACEF7; color: #FFF; }", 0);
-  sheet.insertRule("td { padding: 5px; }", 0);
+function makeDataModel() {
+  return {
+    columnDefinition: columnDefinition,
+    rowHeight: 50,
+    numTotalRows: MAX_ROWS,
+    getRowsFunction: getRows
+  };
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  window.model = makeDataModel();
-  insertStyleRules();
-  var demoRootStyle = {
-    border: '1px solid black',
-    width: '85vw',
-    height: '85vh',
-    fontFamily: 'sans-serif',
-    color: '#000',
-    margin: '20px auto'
-  };
-  ReactDOM.render(
-    <div id='demoRoot' style={demoRootStyle}>
-      <SimianGrid
-        rows={model.rows}
-        numTotalRows={model.rows.length}
-        columnDefinition={model.columnDefinition}
-        rowHeight={model.rowHeight}
-      />
-    </div>,
-    document.body
-  );
+    var model = makeDataModel();
+    ReactDOM.render(
+      <div id = 'demoRoot'>
+        <SimianGrid
+          getRowsFunction = {model.getRowsFunction}
+          numTotalRows = {model.numTotalRows}
+          columnDefinition = {model.columnDefinition}
+          rowHeight = {model.rowHeight}
+          pageSize = {1000}
+          numBufferRows = {25}
+        />
+      </div>,
+      document.body
+    );
 });
